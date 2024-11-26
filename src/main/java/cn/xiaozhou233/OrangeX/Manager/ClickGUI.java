fb74651f2408dcb58af530e16e6df278ced42fad
xiaozhou233 <xiaozhou233@vip.qq.com>
2024-11-26 22:31:52 +0800

    [Add] ClickGUI (reserve)

--- src/main/java/cn/xiaozhou233/OrangeX/Manager/ClickGUI.java
+++ src/main/java/cn/xiaozhou233/OrangeX/Manager/ClickGUI.java
@@ -1,8 +1,43 @@
 package cn.xiaozhou233.OrangeX.Manager;
 
-public class ClickGUI {
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.Minecraft;
+import org.lwjgl.input.Keyboard;
+
+public class ClickGUI extends GuiScreen {
+
     public static void renderClickGUI() {
-        // TODO: ClickGUI
-        System.out.println("TEST");
+        Minecraft.getMinecraft().displayGuiScreen(new ClickGUI());
+    }
+
+    @Override
+    public void initGui() {
+    }
+
+    @Override
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        this.drawDefaultBackground();
+
+        // 获取屏幕中心的X和Y坐标
+        int centerX = this.width / 2;
+        int centerY = this.height / 2;
+
+        // 设置文本颜色
+        this.drawString(Minecraft.getMinecraft().fontRendererObj, "ClickGUI尚未完善",
+                centerX,
+                centerY,
+                0xFFFFFF);
+
+        super.drawScreen(mouseX, mouseY, partialTicks);
+    }
+
+    @Override
+    protected void actionPerformed(GuiButton button) {
+    }
+
+    @Override
+    public boolean doesGuiPauseGame() {
+        return false;
     }
 }
